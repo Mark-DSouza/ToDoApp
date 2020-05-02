977df1041a70a0e91d27e1b0bcca57162d0cefdc
Mark D'Souza <markdsouza434@gmail.com>
2020-05-02 19:34:40 +0530

    Add javascript to append todo divs to todo-list

---
+++ app.js
@@ -0,0 +1,43 @@
+// Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+// Event Listeners
+todoButton.addEventListener("click", addTodo);
+
+
+
+
+
+
+// Functions
+function addTodo(event) {
+    // Prevent default
+    event.preventDefault();
+
+    // Todo div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add("todo");
+
+        // Create li
+        const newTodo = document.createElement('li');
+        newTodo.innerText = 'hey';
+        newTodo.classList.add("todo-item");
+        todoDiv.appendChild(newTodo);
+
+        // Create CheckMark Button
+        const completedButton = document.createElement('button');
+        completedButton.innerHTML = '<i class="fas fa-check"></i>';
+        completedButton.classList.add("completed-btn");
+        todoDiv.appendChild(completedButton);
+
+        // Create Trash Button
+        const trashButton = document.createElement('button');
+        trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+        trashButton.classList.add("trash-btn");
+        todoDiv.appendChild(trashButton);
+
+    // Append to list
+    todoList.appendChild(todoDiv);
+}
